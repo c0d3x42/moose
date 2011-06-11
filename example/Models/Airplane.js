ef6993b6035515b636dca8cdd989fbc9d1461bef
Doug Martin <pollen@Pollenwares-MacBook-Pro.local>
2011-06-11 03:29:03 -0500

    Updated docs removed old files

--- example/Models/Airplane.js
+++
@@ -1,9 +0,0 @@
-var moose = require("../../lib");
-
-//assume airplane has been loaded
-module.exports = exports = (Airplane = moose.addModel(moose.getSchema("airplane")));
-
-var fetchType = Airplane.fetchType;
-Airplane.manyToOne("type", {model : "airplane_type",fetchType : fetchType.EAGER,key : {type_id : "id"}});
-Airplane.oneToMany("legs", {model : "leg_instance",key : {type_id : "id"}});
-
--- example/Models/CanLand.js
+++
@@ -1,3 +0,0 @@
-var moose = require("../../lib");
-
-module.exports = exports = moose.addModel(moose.getSchema("can_land"));
